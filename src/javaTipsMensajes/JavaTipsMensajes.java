43de12c3ca1ef31dbc2a7c8ffb93cf915d82936c
ruben <ruben@DESKTOP-GBE3GNV>
2023-11-11 14:09:56 +0100

    Primer codigo

---
+++ src/javaTipsMensajes/JavaTipsMensajes.java
@@ -0,0 +1,56 @@
+package javaTipsMensajes;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.SpringLayout;
+import javax.swing.JButton;
+
+public class JavaTipsMensajes {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					JavaTipsMensajes window = new JavaTipsMensajes();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public JavaTipsMensajes() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 543, 357);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JPanel panel = new JPanel();
+		frame.getContentPane().add(panel, BorderLayout.CENTER);
+		SpringLayout sl_panel = new SpringLayout();
+		panel.setLayout(sl_panel);
+		
+		JButton btnNewButton = new JButton("New button");
+		sl_panel.putConstraint(SpringLayout.WEST, btnNewButton, 67, SpringLayout.WEST, panel);
+		sl_panel.putConstraint(SpringLayout.SOUTH, btnNewButton, -116, SpringLayout.SOUTH, panel);
+		panel.add(btnNewButton);
+	}
+}
